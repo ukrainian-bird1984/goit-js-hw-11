61375e931ad3533bc36ed013929d56d59f489feb
ukrainian-bird84 <143319836+ukrainian-bird84@users.noreply.github.com>
2024-02-09 19:01:47 +0100

    ждлощ

--- src/main.js
+++ src/main.js
@@ -6,24 +6,26 @@ import 'simplelightbox/dist/simple-lightbox.min.css';
 const form = document.querySelector('.form');
 const gallery = document.querySelector('.gallery');
 const loader = document.querySelector('.loader');
+let lightBox;
 
 loader.style.display = 'none';
-const searchParams = {
-    key: '42200022-9c7e7676f0f903944c054771a',
-    image_type: 'photo',
-    orientation: 'horizontal',
-    safesearch: true,
-    q: '',
-}
 
 form.addEventListener('submit', e => {
     e.preventDefault();
     loader.style.display = 'block';
+
+    gallery.innerHTML = '';
+
     const inputValue = e.target.elements.input.value;
     searchParams.q = inputValue;
+
     getPhotoByName()
         .then(images => createGallery(images))
-        .catch(error => console.log(error))
+        .catch(error => {
+            console.log(error);
+            loader.style.display = 'none';
+        });
+
     e.target.reset();
 });
 
@@ -31,17 +33,17 @@ function getPhotoByName() {
     const urlParams = new URLSearchParams(searchParams);
     return fetch(`https://pixabay.com/api/?${urlParams}`)
         .then(res => {
-        if (res.ok) {
-            return res.json();
-        } else {
-            throw new Error(res.status);
-        }
-    })
+            if (res.ok) {
+                return res.json();
+            } else {
+                throw new Error(res.status);
+            }
+        });
 }
 
 function createGallery(images) {
     if (images.hits.length === 0) {
-       iziToast.show({
+        iziToast.show({
             message: 'Sorry, there are no images matching your search query. Please try again!',
             messageColor: '#FFFFFF',
             backgroundColor: '#EF4040',
@@ -49,39 +51,43 @@ function createGallery(images) {
             messageSize: '16px',
             messageLineHeight: '24px',
             maxWidth: '432px',
-       });
-        gallery.innerHTML = '';
+        });
     } else {
         const link = images.hits.map(image => `<a class="gallery-link" href="${image.largeImageURL}">
-        <img class="gallery-image"
-        src="${image.webformatURL}"
-        alt="${image.tags}"
-         </a>
-        <div class="img-content">
-        <div>
-        <h3>Likes</h3>
-        <p>${image.likes}</p>
-        </div>
+            <img class="gallery-image"
+                src="${image.webformatURL}"
+                alt="${image.tags}"
+            </a>
+            <div class="img-content">
+                <div>
+                    <h3>Likes</h3>
+                    <p>${image.likes}</p>
+                </div>
 
-        <div>
-        <h3>Views</h3>
-        <p>${image.views}</p>
-        </div>
+                <div>
+                    <h3>Views</h3>
+                    <p>${image.views}</p>
+                </div>
 
-        <div>
-        <h3>Comments</h3>
-        <p>${image.comments}</p>
-        </div>
+                <div>
+                    <h3>Comments</h3>
+                    <p>${image.comments}</p>
+                </div>
 
-        <div>
-        <h3>Downloads</h3>
-        <p>${image.downloads}</p>
-        </div>
-        </div>
-        `).join('');
+                <div>
+                    <h3>Downloads</h3>
+                    <p>${image.downloads}</p>
+                </div>
+            </div>`).join('');
+
         gallery.innerHTML = link;
+
+        if (lightBox) {
+            lightBox.refresh();
+        } else {
+            lightBox = new SimpleLightbox('.gallery-link');
+        }
     }
-    let lightBox = new SimpleLightbox('.gallery-link');
-    lightBox.refresh();
+
     loader.style.display = 'none';
 }
